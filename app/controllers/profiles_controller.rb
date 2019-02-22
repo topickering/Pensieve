dbb6ddae2c927a3d515807592973d5e6be64439e
topickering <topickering@gmail.com>
2019-02-22 16:25:17 +0000

    Tidying up

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,7 @@
 class ProfilesController < ApplicationController
+    # before_action :authenticate_user!
+
+    def index
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :profiles
+
   devise_for :users
 
   get 'welcome/index'
